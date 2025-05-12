d50b2ce46a0c1af37be137b09b38032d3743de6d
Mohammad Shariq <mohammad.shariq@researchandranking.com>
2025-05-12 21:42:24 +0530

    Initial Commits

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Mohammad Shariq on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUILearningTests/SwiftUILearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningTests.swift
+//  SwiftUILearningTests
+//
+//  Created by Mohammad Shariq on 12/05/25.
+//
+
+import Testing
+@testable import SwiftUILearning
+
+struct SwiftUILearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
